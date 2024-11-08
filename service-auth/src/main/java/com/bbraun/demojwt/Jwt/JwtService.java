fcc9c17f50f603da4c436b217273c41815cb6b00
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2024-11-08 10:44:59 -0500

    add claims jwt

--- service-auth/src/main/java/com/bbraun/demojwt/Jwt/JwtService.java
+++ service-auth/src/main/java/com/bbraun/demojwt/Jwt/JwtService.java
@@ -21,7 +21,14 @@ public class JwtService {
     private static final String SECRET_KEY="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
 
     public String getToken(UserDetails user) {
-        return getToken(new HashMap<>(), user);
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("email", user.getUsername());
+        extraClaims.put("roles", user.getAuthorities().stream()
+                .map(role -> role.getAuthority())
+                .toList());
+        extraClaims.put("token_type", "Bearer");
+
+        return getToken(extraClaims, user);
     }
 
     private String getToken(Map<String,Object> extraClaims, UserDetails user) {
