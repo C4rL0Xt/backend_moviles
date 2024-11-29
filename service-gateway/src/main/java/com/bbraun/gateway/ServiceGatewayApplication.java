01cef6ed957197055aa22868794fbee24dc937c0
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2024-11-29 15:52:58 -0500

    aaa

--- service-gateway/src/main/java/com/bbraun/gateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/bbraun/gateway/ServiceGatewayApplication.java
@@ -2,10 +2,7 @@ package com.bbraun.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
+
 
 
 @SpringBootApplication
